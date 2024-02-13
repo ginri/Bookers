29d6b92680a57e11b02acdf87e9204b9c9d4bd2f
EC2 Default User <ec2-user@ip-172-31-44-155.ap-northeast-1.compute.internal>
2024-02-13 10:11:24 +0000

    Initial Commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,17 @@
+module ApplicationHelper
+#  def index?
+#    controller.action_name == 'index'
+#  end
+
+#  def show?
+#    controller.action_name == 'show'
+#  end
+
+#  def new?
+#    controller.action_name == 'new'
+#  end
+
+#  def edit?
+#    controller.action_name == 'edit'
+#  end
+end
